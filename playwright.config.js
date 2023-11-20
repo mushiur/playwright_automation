7b9c733cd8bb79f8299250e83dc5836ee3bce895
Md. Mushiur Rahman <mushiur.rahman@sslwireless.com>
2023-11-20 13:14:31 +0600

    files updated

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,8 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'on'
+    screenshot: 'only-on-failure',
+    video: "retain-on-failure"
   },
 
   /* Configure projects for major browsers */
